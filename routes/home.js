d84b84d00508111bcbb3d068823d750c85afd5a0
Yassine Cherradi <yassine.cherradi@yahoo.com>
2021-01-05 19:21:56 +0100

    working on home route

--- routes/home.js
+++ routes/home.js
@@ -3,4 +3,27 @@ const { asyncHandler, csrfProtection, db } = require("./utils");
 
 const router = express.Router();
 
+
+
+router.get('/', asyncHandler(async (req, res, next) => {
+  const userId = await parseInt(req.session.auth.userId);
+  const userGroupsQuery = await db.User.findByPk(userId, {
+    include: [{
+      model: db.Group,
+      through: { attributes: []},
+      
+    }]
+  })
+  const userGroups = userGroupsQuery.Groups
+  const userProjectsQuery = await Promise.all(userGroups.map(async (group) => {
+    const projectsQuery = await db.Group.findByPk(group.id, {
+      include: [{ model: db.Project }]
+    })
+    return projectsQuery;
+  }));
+  res.json(userGroups, userProjectsQuery);
+
+}));
+
+
 module.exports = router;
