d0ac285101331312d5e072a0363385bd2eb395da
William Vincent <compelledtograce@gmail.com>
2021-01-05 14:06:22 -0500

    finished get route for home

--- routes/home.js
+++ routes/home.js
@@ -3,27 +3,46 @@ const { asyncHandler, csrfProtection, db } = require("./utils");
 
 const router = express.Router();
 
+router.get(
+  "/",
+  asyncHandler(async (req, res, next) => {
+    const userId = await parseInt(req.session.auth.userId);
+    const userGroupsQuery = await db.User.findByPk(userId, {
+      include: [
+        {
+          model: db.Group,
+          attributes: ["name"],
+          through: { attributes: [] },
+          include: [
+            {
+              model: db.Project,
+              attributes: ["name", "description", "deadline"],
+              include: [
+                {
+                  model: db.Task,
+                  attributes: ["name", "deadline", "importance", "isComplete"],
+                  include: [
+                    {
+                      model: db.Tag,
+                      attributes: ["name"],
+                      through: { attributes: [] },
+                    },
+                  ],
+                },
+                {
+                  model: db.Category,
+                  attributes: ["name"],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    });
 
-
-router.get('/', asyncHandler(async (req, res, next) => {
-  const userId = await parseInt(req.session.auth.userId);
-  const userGroupsQuery = await db.User.findByPk(userId, {
-    include: [{
-      model: db.Group,
-      through: { attributes: []},
-      
-    }]
+    const groups = userGroupsQuery.Groups;
+    res.json(groups);
   })
-  const userGroups = userGroupsQuery.Groups
-  const userProjectsQuery = await Promise.all(userGroups.map(async (group) => {
-    const projectsQuery = await db.Group.findByPk(group.id, {
-      include: [{ model: db.Project }]
-    })
-    return projectsQuery;
-  }));
-  res.json(userGroups, userProjectsQuery);
-
-}));
-
+);
 
 module.exports = router;
