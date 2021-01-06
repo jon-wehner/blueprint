0c7eb2e85b2977afc119696f4520fe773d299d00
William Vincent <compelledtograce@gmail.com>
2021-01-06 14:09:31 -0500

    finished project edit functionality

--- public/javascripts/buttons.js
+++ public/javascripts/buttons.js
@@ -13,7 +13,7 @@ groupEditButtons.forEach((button) =>
     <h3>Edit Group</h3>
     <div>
       <input type="hidden" name="_csrf" value=${csrfToken}>
-      <label>Group Name:</label>
+      <label>Name:</label>
       <input name="name" type="text" value=${groupName}>
     </div>
     <input type="submit" value="Update Group">
@@ -24,18 +24,40 @@ groupEditButtons.forEach((button) =>
 projectEditButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
     const projectId = e.target.id;
-    const projectName = e.target.value;
     const csrfToken = e.target.dataset.token;
+    const categories = e.target.dataset.categories;
+    const parsedCategories = JSON.parse(categories);
 
     taskArea.innerHTML = `
-    <form action="/home/projects/${projectId}/edit">
+    <form action="/home/projects/${projectId}/edit" method="post">
       <h3>Edit Project</h3>
       <div>
-        <input type="hidden" name="_csrf" value=${csrfToken}>
-        <label>Project Name:</label>
-        <input type="text name="name" value=${projectName}>
-        <></label>
+        <input type="hidden" name="_csrf" value="${csrfToken}">
+
+        <label>Name:</label>
+        <input type="text" name="name">
+
+        <label>Description:</label>
+        <textarea name="description"></textarea>
+
+        <label>Deadline:</label>
+        <input type="date" name="deadline">
+
+        <label>Category</label>
+        <select name="categoryId" id="categorySelect"></select>
       </div>
+      </div>
+      <input type="submit" value="Update Project">
     </form>`;
+
+    const selectMenu = document.getElementById("categorySelect");
+
+    Array.from(parsedCategories).forEach((category) => {
+      console.log(category);
+      const newOption = document.createElement("option");
+      newOption.textContent = category.name;
+      newOption.value = category.id;
+      selectMenu.appendChild(newOption);
+    });
   });
 });
