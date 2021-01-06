babe0a5b18cb0dd5247f64b47c7e87cc71455423
William Vincent <compelledtograce@gmail.com>
2021-01-06 17:40:03 -0500

    changed login form to utilize pug templates

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,55 +1,31 @@
 const signupSelect = document.querySelector(".signup-text");
 const loginSelect = document.querySelector(".login-text");
+
 const formContainer = document.querySelector(".form-container");
 const submitButton = document.querySelector(".submit-btn");
-const form = document.querySelector(".form");
-const demoButton = document.getElementById("demo-btn");
 
-loginSelect.addEventListener("click", function () {
-  this.classList.add("active-selection");
-  this.classList.remove("inactive-selection");
+const form = document.querySelector(".form");
+const signupForm = document.getElementById("signup-form");
+const loginForm = document.getElementById("login-form");
+
+loginSelect.addEventListener("click", () => {
+  loginSelect.classList.add("active-selection");
+  loginSelect.classList.remove("inactive-selection");
+
   signupSelect.classList.add("inactive-selection");
   signupSelect.classList.remove("active-selection");
-  demoButton.classList.remove("hidden");
-  formContainer.innerHTML = `
-  <div class="form-item">
-  <label for="email"> Email:</label>
-  <input type="text" name="email" />
-</div>
-<div class="form-item">
-  <label for="password"> Password:</label>
-  <input type="password" name="password" />
-</div>
-  `;
-  submitButton.innerText = "Log In";
-  form.setAttribute("action", "/users/login");
-  form.setAttribute("method", "POST");
+
+  loginForm.classList.remove("hidden");
+  signupForm.classList.add("hidden");
 });
 
-signupSelect.addEventListener("click", function () {
-  this.classList.add("active-selection");
-  this.classList.remove("inactive-selection");
+signupSelect.addEventListener("click", () => {
+  signupSelect.classList.add("active-selection");
+  signupSelect.classList.remove("inactive-selection");
+
   loginSelect.classList.add("inactive-selection");
   loginSelect.classList.remove("active-selection");
-  demoButton.classList.add("hidden");
-  formContainer.innerHTML = `
-  <div class="form-item">
-  <label for="username"> Username:</label>
-  <input type="text" name="username" />
-</div>
-<div class="form-item">
-  <label for="email"> Email:</label>
-  <input type="email" name="email" />
-</div>
-<div class="form-item">
-  <label for="password"> Password:</label>
-  <input type="password" name="password" />
-</div>
-<div class="form-item">
-  <label for="confirmPassword"> Confirm Password:</label>
-  <input type="password" name="confirmPassword" />
-</div>
-  `;
-  submitButton.innerText = "Create Account";
-  form.setAttribute("action", "/users/signup");
+
+  loginForm.classList.add("hidden");
+  signupForm.classList.remove("hidden");
 });
