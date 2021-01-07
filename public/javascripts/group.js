d80f3ac76a56559694e6150e902802b2fbac026f
Yassine Cherradi <yassine.cherradi@yahoo.com>
2021-01-07 19:26:00 +0100

    add project button completed and fixed double rendering of forms

--- public/javascripts/group.js
+++ public/javascripts/group.js
@@ -1,7 +1,11 @@
 const addGroupButton = document.getElementById("add-group");
 const addGroupForm = document.getElementById("addGroup")
+const forms = document.querySelectorAll('.form');
+const formsArray = Array.from(forms);
 addGroupButton.addEventListener('click', async () => {
-  // await fetch('/home/groups', {method: 'POST', body: JSON.stringify({name})}
+  formsArray.forEach((form) => {
+    form.classList.add('hidden-form');
+  })
   addGroupForm.classList.remove('hidden-form');
   
 });
