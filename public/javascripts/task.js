7f57e38afdaed25dc8b62e5d23d773d40101cd16
jon-wehner <jonjwehner@gmail.com>
2021-01-07 10:09:00 -0800

    task node and delete button now removes from DOM

--- public/javascripts/task.js
+++ public/javascripts/task.js
@@ -1,6 +1,8 @@
 const tasksArea = document.querySelector(".tasks-area");
 const taskListItems = document.querySelectorAll(".project-task-list-item");
 const taskDeleteButtons = document.querySelectorAll(".task-delete-btn");
+const taskForm = document.getElementById("addEditTask")
+const addTaskBtns = document.querySelectorAll(".add-task-button")
 const taskItemsArray = Array.from(taskListItems);
 const deleteButtonsArray = Array.from(taskDeleteButtons);
 
@@ -10,43 +12,23 @@ deleteButtonsArray.forEach(btn => {
       method: "DELETE",
       body: JSON.stringify({ id: btn.dataset.id }),
     });
+    const task = document.getElementById(`task-${btn.dataset.id}`)
+    task.remove()
+    btn.remove()
   });
 });
 
+//Shows and hides the task form to edit a task when clicked
 taskItemsArray.forEach(task => {
   task.addEventListener("click", e => {
-    tasksArea.innerHTML = `<form>
-    <h3>Edit Task</h3>
-    <div>
-      <label>Task Name:</label>
-      <input type="text" id="edited-task-name" value="${task.innerText}"/>
-    </div>
-    <div>
-      <label>Due Date:</label>
-      <input type="date" id="edited-task-date" />
-    </div>
-    <div>
-      <p>Importance Level:</p>
-      <div>
-        <input type="radio" name="importance-level" checked />
-        <label for="huey">Regular</label>
-      </div>
-
-      <div>
-        <input type="radio" name="importance-level" />
-        <label for="dewey">Priority</label>
-      </div>
-
-      <div>
-        <input type="radio" name="importance-level" />
-        <label for="louie">Urgent</label>
-      </div>
-    </div>
-    <div>
-      <label>Completed?</label>
-      <input type="checkbox" id="edited-task-completion-status" />
-    </div>
-    <button>Update Task</button>
-  </form>`;
+    taskForm.classList.toggle("hidden-form")
   });
 });
+
+//Shows and Hides the form when "Add Task" is clicked
+addTaskBtns.forEach(btn => {
+  btn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    taskForm.classList.toggle("hidden-form")
+  })
+})
