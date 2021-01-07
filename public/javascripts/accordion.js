716f9fb93b845acb7581e7386021c2cc9f38eafd
William Vincent <compelledtograce@gmail.com>
2021-01-07 16:33:35 -0500

    working on right section for form interaction

--- public/javascripts/accordion.js
+++ public/javascripts/accordion.js
@@ -1,13 +1,42 @@
-const accordian = document.getElementsByClassName("accordion");
+const projects = document.querySelectorAll(".accordion");
+const projectDescription = document.getElementById("project-description");
 
-for (let i = 0; i < accordian.length; i++) {
-  accordian[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    let panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
+projects.forEach((project) => {
+  project.addEventListener("click", async (e) => {
+    const projectId = await parseInt(e.target.dataset.id);
+    const panel = project.nextElementSibling;
+    project.classList.toggle("active");
+
+    panel.style.display === "block"
+      ? (panel.style.display = "none")
+      : (panel.style.display = "block");
+
+    const projectDetails = document.getElementById("project-details");
+    const projectCategory = document.getElementById("project-category-p");
+    const projectDescription = document.getElementById("project-description-p");
+    const projectDeadline = document.getElementById("project-deadline-p");
+    const taskSummary = document.getElementById("project-task-summary-p");
+    const editProjectBtn = document.querySelector(".project-edit-button");
+
+    const response = await fetch(`/api/projects/${projectId}/tasks`);
+    const projectJson = await response.json();
+    const category = projectJson.Category.name;
+
+    const tasks = projectJson.Tasks;
+    const totalTasks = tasks.length;
+    let completedCount = 0;
+    tasks.forEach((task) => {
+      if (task.isComplete) completedCount++;
+    });
+
+    projectCategory.innerHTML = category;
+    projectDescription.innerHTML = projectJson.description;
+    projectDeadline.innerHTML = projectJson.deadline;
+    taskSummary.innerHTML = `Completed Tasks: ${completedCount}/${totalTasks}`;
+
+    editProjectBtn.setAttribute("id", projectId);
+    editProjectBtn.setAttribute("value", projectJson.name);
+
+    projectDetails.classList.remove("hidden");
   });
-}
+});
--- public/javascripts/buttons.js
+++ public/javascripts/buttons.js
@@ -1,36 +1,35 @@
 const groupEditButtons = document.querySelectorAll(".group-edit-button");
 const projectEditButtons = document.querySelectorAll(".project-edit-button");
 const taskArea = document.querySelector(".tasks-area");
-const editGroupForm = document.getElementById("editGroup")
-const editProjectForm = document.getElementById("editProject")
-const groupNameField = document.getElementById("groupName")
-const projectNameField = document.getElementById("projectNameField")
-const taskAreaForms = document.querySelectorAll(".task-area-forms")
+const editGroupForm = document.getElementById("editGroup");
+const editProjectForm = document.getElementById("editProject");
+const groupNameField = document.getElementById("groupName");
+const projectNameField = document.getElementById("projectNameField");
+const taskAreaForms = document.querySelectorAll(".task-area-forms");
 
 groupEditButtons.forEach((button) =>
   button.addEventListener("click", (e) => {
     const groupId = e.target.id;
     const groupName = e.target.value;
 
-    editGroupForm.action=`/home/groups/${groupId}/name`
-    groupNameField.value = groupName
-    editGroupForm.classList.toggle("hidden-form")
+    editGroupForm.action = `/home/groups/${groupId}/name`;
+    groupNameField.value = groupName;
+    editGroupForm.classList.toggle("hidden-form");
   })
 );
 
 projectEditButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
     const projectId = e.target.id;
-    const projectName = e.target.value
-    editProjectForm.action = `/home/projects/${projectId}/edit`
-    projectNameField.value = projectName
-
-    editProjectForm.classList.toggle("hidden-form")
+    const projectName = e.target.value;
+    editProjectForm.action = `/home/projects/${projectId}/edit`;
+    projectNameField.value = projectName;
+    editProjectForm.classList.toggle("hidden-form");
   });
 });
 
 taskAreaForms.forEach((form) => {
   form.addEventListener("submit", (e) => {
-    form.classList.toggle("hidden-form")
-  })
-})
+    form.classList.toggle("hidden-form");
+  });
+});
