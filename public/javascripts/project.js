98a69da4271f8c6bdb3d7c0c4ba6438379c1e375
jon-wehner <jonjwehner@gmail.com>
2021-01-07 18:36:35 -0800

    adding projects now functions

--- public/javascripts/project.js
+++ public/javascripts/project.js
@@ -2,7 +2,25 @@ const addProjectButton = document.getElementById("add-project");
 const addProjectForm = document.getElementById("addProject");
 const forms = document.querySelectorAll(".task-area-forms");
 
-addProjectButton.addEventListener("click", async () => {
+const postForm = async (url, formData, httpMethod) => {
+  const formPlainObj = Object.fromEntries(formData.entries());
+  const formJson = JSON.stringify(formPlainObj);
+  console.log(formJson)
+  const response = await fetch(url, {
+    method: httpMethod,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: formJson,
+  });
+  if (!response.ok) {
+    //TODO Error Handling
+  }
+  return response;
+};
+
+
+addProjectButton.addEventListener("click", () => {
   forms.forEach((form) => {
     form.classList.add("hidden-form");
   });
