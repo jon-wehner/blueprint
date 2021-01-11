cf705d58b1e7de16fdda1057f778cb18f210633c
rob <darabandev@gmail.com>
2021-01-10 21:27:56 -0500

    Removed change to app.js

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
   res.status(err.status || 500);
-  res.render("error", { err });
+  res.render("error");
 });
 
 module.exports = app;
